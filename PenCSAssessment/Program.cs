af3d0ede72d7976461efa559076b835fb95340e6
Hadi Jalilvand <h.Jalilvand2010@gmail.com>
2021-09-15 14:51:03 +1000

    initial commit

---
+++ PenCSAssessment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using PenCSAssessment.Questions;
+
+namespace PenCSAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 10, 30, 50 };
+
+
+            Console.WriteLine(Assessment.Average(list));
+            //-------------------------------------------------------
+
+            List<Coordinate> coordinateList = new List<Coordinate>();
+            coordinateList.Add(new Coordinate() { X = 1, Y = 2 });
+            coordinateList.Add(new Coordinate() { X = 3, Y = 4 });
+            coordinateList.Add(new Coordinate() { X = 1, Y = 2 });
+
+            coordinateList = Assessment.GetMatchingCoordinates(coordinateList, 1, 2);
+            Console.WriteLine($"Coordinate = {coordinateList.ToArray()[0].X} , {coordinateList.ToArray()[0].Y} , Count: {coordinateList.ToArray().Length}");
+            //----------------------------------------------------------------------------------------------------------------------------------------------------
+
+            List<string> strings = new List<string> { "Michael", "Sandy", "Cheran", "Rashmi", "Markand" };
+            char[] toRemove = new char[] { 'M', 'i', 'c', 'a', 'l', 'h', 'e' };
+            strings = Assessment.RemoveCharacters(strings, toRemove);
+
+            strings.ForEach(f => 
+            {
+                Console.WriteLine(f + ",");
+            });
+
+            Assessment.TrickQuestion();
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ PenCSAssessment/Questions/Assessment.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PenCSAssessment.Questions
+{
+    public static class Assessment
+    {
+        /// <summary>
+        /// Implement this method that takes a list of integers and returns the average
+        /// </summary>
+        public static double Average(List<int> list)
+        {
+            double sum = 0;
+            list.ForEach(f =>
+            {
+                sum += f;
+            });
+            return sum / list.Count;
+        }
+
+        /// <summary>
+        /// Implement this method that takes a list of Coordinate objects (see below) and 
+        /// returns a list of Coordinate objects with those in the input list not matching int /// x and int y filtered out.
+        /// </summary>
+        public static List<Coordinate> GetMatchingCoordinates(List<Coordinate> coord, int x, int y)
+        {
+
+            return coord.Where(i => i.X == x && i.Y == y).ToList();
+
+        }
+
+
+        /// <summary>
+        /// Implement this method that takes a list of string and replaces any characters
+        /// in those strings that are in toRemove with a '*' character. Any strings in the 
+        /// list
+        /// that have all of their characters replaced with * should be removed from the list.
+        /// ie, the input of:
+        ///     strings = { "Michael", "Sandy", "Cheran", "Rashmi", "Markand" };
+        ///     toRemove = ['M', 'i', 'c', 'a', 'l', 'h', 'e'];
+        /// should return:
+        ///     { "S*ndy", "C**r*n", "R*s*m*", "**rk*nd" }
+        /// </summary>
+        public static List<string> RemoveCharacters(List<string> strings, char[] toRemove)
+        {
+            List<string> result = new List<string>();
+            strings.ForEach(f =>
+            {
+                int count = 0;
+               
+                for (int i = 0; i < toRemove.Length; i++)
+                {
+                    if (f.Contains(toRemove[i]))
+                    {
+                       f = f.Replace(toRemove[i], '*');
+                        count++;
+                    }
+                }
+                if (count != f.Length)
+                {
+                    result.Add(f);
+                }
+               
+            });
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Write the output to the console of the following method
+        /// </summary>
+        public static void TrickQuestion()
+        {
+            int[] inOrderArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Parallel.ForEach(inOrderArray, i => Console.Write(i));
+        }
+
+
+
+
+    }
+    public class Coordinate
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
